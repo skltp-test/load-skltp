52f1139d07cdfdd441af601c0a9ba3c42852dded
Frank Grundin <frank.grundin@nordicmedtest.se>
2019-01-08 14:37:45 +0100

    Debug added

--- loadtest/user-files/chown-files.sh
+++ loadtest/user-files/chown-files.sh
@@ -4,7 +4,9 @@
 #For the directory of this script, set ownership of all files to same as directory
 #------------------------------------
 DIR=$(dirname `readlink -f $0`)
+echo "Ownership dir: ${DIR}"
 DIR_OWNER=$(stat -c "%u:%g" $DIR)
+echo "Ownership: ${DIR_OWNER}"
 set -x
 chown -R $DIR_OWNER $DIR
 
