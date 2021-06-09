dcb65142fcebdbfac4f981dc0981fda753bd34a8
Björn Pettersson <bjorn.pettersson@nordicmedtest.se>
2021-06-09 11:58:28 +0200

    Update-test parametrar

--- loadtest/user-files/simulations/RequestsUpdate.scala
+++ loadtest/user-files/simulations/RequestsUpdate.scala
@@ -5,13 +5,18 @@ import io.gatling.http.Predef._
 import scala.concurrent.duration._
 
 class RequestsUpdate extends Simulation {
+        // Input paramters
+        val targetHost = sys.env("TARGETHOST")
+        val rps = sys.env("RPS").toInt
+        val update1000Pause = sys.env("UPDATE1000_PAUSE").toInt
+        val update20Users = sys.env("UPDATE20_USERS").toInt
 
         // Traffic will be routed toward LOAD-MOCKS based choosen personId
 	val feeder1 = csv("../user-files/data/ViaAGP.csv").circular
 	val feederUpdate = csv("../user-files/data/Update.csv").circular
 	
 	val httpConf = http
-		.baseUrl("https://TARGETHOST/vp")
+		.baseUrl(s"https://$targetHost/vp")
 		.acceptEncodingHeader("gzip, deflate")
 		.userAgentHeader("SKLTP load tests")
 
@@ -31,16 +36,16 @@ class RequestsUpdate extends Simulation {
                 .check(status.is(200), responseTimeInMillis.lt(35000)))
 
         val requestGetDiagnosis_2 = scenario("GetDiagnosis_2").feed(feeder1).exec(GetDiagnosis_2)
-        val requestUpdate20 = scenario("Update20").feed(feederUpdate).exec(Update20).pause(10,30)
-        val requestUpdate1000 = scenario("Update1000").feed(feederUpdate).exec(Update1000).pause(5)
+        val requestUpdate20 = scenario("Update20").feed(feederUpdate).exec(Update20).pause(5,15)
+        val requestUpdate1000 = scenario("Update1000").feed(feederUpdate).exec(Update1000).pause(update1000Pause)
 
-  // Simulera grundladdning med 1000 poster/anrop, 5 sek paus mellan
-  // + Mindre Update-anrop med 10-30 sek paus mellan
-  // + Trafik via AgP, 1 anrop/s
+  // Simulera grundladdning med 1000 poster/anrop, varierbar paus mellan
+  // + Mindre Update-anrop med 5-15 sek paus mellan
+  // + Trafik via AgP
   setUp(
     requestUpdate1000.inject(constantConcurrentUsers(1) during (20 minutes)), 
-    requestUpdate20.inject(constantConcurrentUsers(1) during (20 minutes)), 
-    requestGetDiagnosis_2.inject(constantUsersPerSec(1) during(20 minutes)))
+    requestUpdate20.inject(constantConcurrentUsers(update20Users) during (20 minutes)), 
+    requestGetDiagnosis_2.inject(constantUsersPerSec(rps) during(20 minutes)))
     .protocols(httpConf)  
     .assertions(
       global.successfulRequests.percent.gt(99),
