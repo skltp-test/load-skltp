cb4f3de16f092811bea910cabf09f12d9af8c5d7
Björn Pettersson <bjorn.pettersson@nordicmedtest.se>
2021-09-13 14:46:05 +0200

    Distributed test

---
+++ loadtest/user-files/run-test-dist.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#------------------------------------
+# Run Load test (mute cli questions)
+#------------------------------------
+
+gatling.sh -nr -s $1
+
+
+#------------------------------------
+#For the directory of this script, set ownership of all files to same as directory
+#------------------------------------
+DIR=$(dirname `readlink -f $0`)
+echo "Ownership dir: ${DIR}"
+DIR_OWNER=$(stat -c "%u:%g" $DIR)
+echo "Ownership: ${DIR_OWNER}"
+set -x
+chown -R $DIR_OWNER $DIR
