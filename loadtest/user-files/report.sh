42736f847089f6d7854ce5eb06e4a479a08fe99d
Björn Pettersson <bjorn.pettersson@nordicmedtest.se>
2021-09-13 17:22:46 +0200

    distribute

--- loadtest/user-files/report.sh
+++ loadtest/user-files/report.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 
 #------------------------------------
-# Run Load test (mute cli questions)
-#------------------------------------
-
-gatling.sh -ro all
-
-
-#------------------------------------
 #For the directory of this script, set ownership of all files to same as directory
 #------------------------------------
 DIR=$(dirname `readlink -f $0`)
@@ -16,3 +9,12 @@ DIR_OWNER=$(stat -c "%u:%g" $DIR)
 echo "Ownership: ${DIR_OWNER}"
 set -x
 chown -R $DIR_OWNER $DIR
+
+#------------------------------------
+# Run Load test (mute cli questions)
+#------------------------------------
+
+gatling.sh -ro all
+
+# results folder has a new directory
+chown -R $DIR_OWNER $DIR/../results
